2b62b89ade24cf8efe7c0f1b508c102d116fc4c0
pjgowtham <pjgowtham@gmail.com>
2022-11-25 21:17:48 +0530

    sde_kms: Avoid delaying before plane cleanup or vync in recovery mode
    Fixes the issue where only blank screen is displayed when booting
    into recovery on realme Q3s

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -74,6 +74,8 @@
 #include "oplus_adfr.h"
 #endif
 
+#include <soc/oplus/boot_mode.h>
+
 /* defines for secure channel call */
 #define MEM_PROTECT_SD_CTRL_SWITCH 0x18
 #define MDP_DEVICE_ID            0x1A
@@ -1615,7 +1617,7 @@ static void sde_kms_wait_for_commit_done(struct msm_kms *kms,
 		 */
 		SDE_EVT32_VERBOSE(DRMID(crtc));
 		ret = sde_encoder_wait_for_event(encoder, MSM_ENC_COMMIT_DONE);
-		if (ret && ret != -EWOULDBLOCK) {
+		if ((MSM_BOOT_MODE__RECOVERY != get_boot_mode()) && (ret && ret != -EWOULDBLOCK)) {
 			SDE_ERROR("wait for commit done returned %d\n", ret);
 			sde_crtc_request_frame_reset(crtc, encoder);
 			break;
